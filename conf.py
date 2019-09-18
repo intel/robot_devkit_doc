b75841f77ddf605e04fa2e52cdb57230a937a052
ahuizxc <shenghui.liu@intel.com>
2019-09-18 16:03:55 +0800

    fix version error

--- conf.py
+++ conf.py
@@ -23,9 +23,9 @@ copyright = u'2019, Intel Corporation'
 author = u'shenghui'
 
 # The short X.Y version
-version = u'0.1.1'
+version = u'0.2.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.1'
+release = u'0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
